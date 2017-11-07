814aa3662d73480d3ab55e98290a13b18a9459ce
john <john@john-X555LAB>
2017-11-07 21:35:02 +0100

    Se crea el constructor de la clase Posicion

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -2,7 +2,25 @@ package tarea03;
 
 public class Posicion {
 	
-	int fila;
-	char columna;
+	private int fila;
+	private char columna;
+	
+	public Posicion (int fila, char columna) {
+		
+		if((fila >= 1 && fila <= 10) && (columna >= 97 && columna <= 104)) {
+			
+			this.fila = fila;
+			this.columna = columna;
+			
+		}else {
+			
+			System.out.printf("Error, ha introducido valores no válidos o fuera del rango");
+			System.out.printf("Se establecerá el valor \"1\" para la fila y el valor \"a\" para la columna.");
+			this.fila = 1;
+			this.columna = 'a';
+			
+		}
+		
+	}
 
 }
