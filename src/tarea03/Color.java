e8a607c610ef61eb5d652751eac0b485b62ba884
john <john@john-X555LAB>
2017-11-07 20:47:49 +0100

    Se crea el enumerado Direccion

--- src/tarea03/Color.java
+++ src/tarea03/Color.java
@@ -1,5 +1,11 @@
+/**
+ * Los valores de este enumerado simulan los colores del tablero
+ */
+
 package tarea03;
 
 public enum Color {
 
+	BLANCO, NEGRO;
+	
 }
---
+++ src/tarea03/Direccion.java
@@ -0,0 +1,14 @@
+/**
+ * Este enumerado simula los movimientos por el tablero
+ */
+package tarea03;
+
+/**
+ * @author john
+ *
+ */
+public enum Direccion {
+
+	NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE;
+	
+}
