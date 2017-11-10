c01f7cc5d2c9772141feff90caed1cd8899e484f
john <john@john-X555LAB>
2017-11-10 20:53:16 +0100

    Se añaden los comentarios tipo javadoc a la clase y a sus metodos

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -1,5 +1,14 @@
 package tarea03;
 
+
+/**
+ * @author john
+ * 
+ * Creamos la clase posicion y definimos los atributos
+ * fila y columna. No se utilizan tildes en los comentarios
+ * expresamente para evitar errores en la generacion de los Javadoc
+ */
+
 public class Posicion {
 	
 	private int fila;
@@ -7,7 +16,7 @@ public class Posicion {
 	
 	public Posicion (int fila, char columna) {
 		
-		if((fila >= 1 && fila <= 10) && (columna >= 97 && columna <= 104)) {
+		if((fila >= 1 && fila <= 8) && (columna >= 97 && columna <= 104)) {
 			
 			this.fila = fila;
 			this.columna = columna;
@@ -21,10 +30,20 @@ public class Posicion {
 			
 		}
 	}
+	
+	/**
+	 * Creamos los metodos set y get
+	 */
+	
+	/**
+	 * Creamos el metodo setFila y comprobamos que el dato 
+	 * pasado por paramtero se encuentra en el rango 1 - 8
+	 * @param fila
+	 */
 		
 	public void setFila(int fila) {
 			
-		if(fila >= 1 && fila <= 10) {
+		if(fila >= 1 && fila <= 8) {
 				
 			this.fila = fila;
 				
@@ -37,6 +56,12 @@ public class Posicion {
 		}
 			
 	}
+	
+	/**
+	 * Creamos el metodo setColumna y comprobamos que el dato
+	 * pasado por parametro se encuentra entre el rango 'a'-'h'
+	 * @param columna
+	 */
 		
 	public void setColumna(char columna) {
 		
@@ -53,18 +78,33 @@ public class Posicion {
 		}
 			
 	}
-		
+	
+	/**
+	 * Se declara el metodo getFila 
+	 * @return fila
+	 */
+	
 	public int getFila() {
 			
 		return fila;
 			
 	}
+	
+	/**
+	 * Se declara el metodo getColumna
+	 * @return
+	 */
 		
 	public char getColumna() {
 			
 		return columna;
 			
 	}
+	
+	/**
+	 * Declaramos un metodo toString que devuelve las coordenadas donde se
+	 * encuentra el rey
+	 */
 	
 	public String toString() {
 		
