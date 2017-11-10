44864c6789329c6c183c59a14fa00995fa86b7d7
john <john@john-X555LAB>
2017-11-10 22:46:17 +0100

    Se añaden los comentarios tipo javadoc

--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -1,7 +1,12 @@
 package tarea03;
-
+/**
+ * Se crea la clase Rey con los atributos color de tipo Color
+ * y posicion de tipo Posicion
+ * @author john
+ *
+ */
 public class Rey {
-
+	
 	private Color color;
 	private Posicion posicion;
 	
@@ -29,30 +34,50 @@ public class Rey {
 		
 	}
 	
+	/**
+	 * El metodo getColor devuelve una variable de tipo Color
+	 * que indica el color del objeto
+	 * @return color
+	 */
 	public Color getColor() {
 		
 		return color;
 		
 	}
 	
+	/**
+	 * El metodo getPosicion devuelve una variable de tipo Posicion
+	 * que determina la posicion del objeto
+	 * @return posicion
+	 */
 	public Posicion getPosicion() {
 		
 		return posicion;
 		
 	}
 	
+	/**
+	 * Este metodo toString devuelve una cadena 
+	 * que representa el objeto
+	 */
 	public String toString() {
 		
 		return "El rey " + color + " se encuentra en la posición " + posicion;
 		
 	}
 	
+	
+	/**
+	 * El metodo mueve actualiza la posicion del objeto a traves de los parametros
+	 * @param fila
+	 * @param columna
+	 */
 	public void mueve(int fila, char columna) {
 		
 		while((fila < 1 || fila > 8) && (columna < 97 || columna > 104)) {
 		
 			System.out.println("Los parámetros introducidos están fuera del rango establecido.");
-			System.out.print("El valor para la fila debe estar comprendido entre 1 y 8.");
+			System.out.println("El valor para la fila debe estar comprendido entre 1 y 8.");
 			System.out.println("El valor para la columna debe estar comprendido entre \'a' y \'h'.");
 			
 		}
